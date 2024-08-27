0f779ed845c0fe070dc51c07a638650cd7d41a75
priyadarshini-SF4715 <priyadarshini.d@syncfusion.com>
2024-08-27 12:30:45 +0530

    Add files via upload
    Text Area Sample Committed

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { TextAreaComponent as EjsTextarea } from '@syncfusion/ej2-vue-inputs';
+</script>
+
+<template>
+<div class="textarea">
+  <ejs-textarea width="200" :enabled="false" 
+   floatLabelType="Never"
+   rows="3" cols="50" resizeMode="Both"
+   maxLength="8" readOnly="true" value="Comments">
+  </ejs-textarea>
+</div>
+</template>
+
+<style>
+@import "../node_modules/@syncfusion/ej2-base/styles/fluent.css";
+@import "../node_modules/@syncfusion/ej2-vue-inputs/styles/fluent.css";
+.textarea {
+  padding-bottom: 160px;
+}
+</style>
